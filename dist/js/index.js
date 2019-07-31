b5cf6e7a24049260d8a023b984477f0aecd3d108
Hong Baojin <13809735212@163.com>
2019-07-31 18:44:44 +0800

    write to index-yxzd.html

---
+++ dist/js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    // 如果需要分页器
+    pagination: '.swiper-pagination'
+
+    // autoplay: 2000
+  });
+});
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+
+    // autoplay: 2000
+  });
+});
